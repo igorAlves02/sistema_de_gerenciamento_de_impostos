78853673c14f774d345cd806480f4bada2a9d8a4
igor alves <igoralvespalmeirag4@gmail.com>
2025-03-05 21:53:04 -0300

    feat(config): adicionadas dependências ao pom.xml e configurado application.properties

---
+++ src/main/java/br/com/zup/sistema_de_gerenciamento_de_impostos/SistemaDeGerenciamentoDeImpostosApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.sistema_de_gerenciamento_de_impostos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGerenciamentoDeImpostosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGerenciamentoDeImpostosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/sistema_de_gerenciamento_de_impostos/SistemaDeGerenciamentoDeImpostosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.sistema_de_gerenciamento_de_impostos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGerenciamentoDeImpostosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
