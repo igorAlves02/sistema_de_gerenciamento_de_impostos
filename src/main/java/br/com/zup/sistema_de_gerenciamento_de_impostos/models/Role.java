c01c36c42ba757ef65f1f4dc4d5a76217080d624
igor alves <igoralvespalmeirag4@gmail.com>
2025-03-07 12:04:23 -0300

    feat(permições): Feito o enum Role para gerenciar as permições.

---
+++ src/main/java/br/com/zup/sistema_de_gerenciamento_de_impostos/models/Role.java
@@ -0,0 +1,6 @@
+package br.com.zup.sistema_de_gerenciamento_de_impostos.models;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/br/com/zup/sistema_de_gerenciamento_de_impostos/models/User.java
+++ src/main/java/br/com/zup/sistema_de_gerenciamento_de_impostos/models/User.java
@@ -1,5 +1,6 @@
 package br.com.zup.sistema_de_gerenciamento_de_impostos.models;
 
+import br.com.zup.sistema_de_gerenciamento_de_impostos.models.Role;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
