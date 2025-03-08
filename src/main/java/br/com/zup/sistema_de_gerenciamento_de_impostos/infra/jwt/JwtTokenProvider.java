ceea6ef5895d37f44b38825c795c1d8250a08ab6
igor alves <igoralvespalmeirag4@gmail.com>
2025-03-08 01:32:38 -0300

    feat(jwt): implementação do JwtTokenProvider e configuração no application.properties, para que fique mais seguro, e as informações não fiquem soltas no código

---
+++ src/main/java/br/com/zup/sistema_de_gerenciamento_de_impostos/infra/jwt/JwtTokenProvider.java
@@ -0,0 +1,61 @@
+package br.com.zup.sistema_de_gerenciamento_de_impostos.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+    
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpirationMs; // Expiração do token
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + jwtExpirationMs);
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(expireDate)
+                .signWith(SignatureAlgorithm.HS256, key())
+                .compact();
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build()
+                .parse(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
